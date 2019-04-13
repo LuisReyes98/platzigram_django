76b2030b2dc695044e829e5a897549da11737c14
Luis Reyes <luisrogelio98@gmail.com>
2019-04-13 16:48:01 -0400

    clase 25

---
+++ platzigram/middleware.py
@@ -0,0 +1,31 @@
+""" Platzigram user imafe middleware """
+
+#Django
+from django.shortcuts import redirect
+
+from django.urls import reverse
+
+class ProfileCompletitionMiddleWare:
+  """Profile Completition MiddleWare 
+    Ensure every user has profile picture and biography
+    Redirecting them to chose one if they don't have one
+  """
+
+  def __init__(self,get_response):
+    """Middleware initialization"""
+    self.get_response = get_response
+
+
+
+  def __call__(self,request):
+    """Code to be executed for each request before the views is called"""
+
+    if not request.user.is_anonymous:
+      if not request.user.is_staff:
+        profile = request.user.profile
+        if not profile.picture or not profile.biography:
+          if request.path not in [reverse('update_profile'), reverse('logout')]:
+            return redirect('update_profile')    
+    
+    response = self.get_response(request)
+    return response
--- platzigram/urls.py
+++ platzigram/urls.py
@@ -31,7 +31,7 @@ from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('', local_views.hello_world), #root
+    path('', posts_views.list_posts, name='feed'),  # root
 
     path('hello-world/', local_views.hello_world, name='hello_world'),
 
@@ -43,6 +43,11 @@ urlpatterns = [
     path('users/login/', users_views.login_view, name='login'),
 
     path('users/logout/', users_views.logout_view, name='logout'),
+
+    path("users/signup/", users_views.signup, name="signup"),
+
+    path("users/me/profile", users_views.update_profile, name="update_profile"),
+    
 
 
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
