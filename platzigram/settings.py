6f1d3cbc0c5db97d80e5d0822dd2ae11abfae046
Luis Reyes <luisrogelio98@gmail.com>
2019-04-20 18:09:54 -0400

    postgresql config

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -84,9 +84,17 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_database_platzigram',
+        'USER': 'luis',
+        'PASSWORD': '1998',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,10 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+# Los models son la ORM de django
+# ORM (Object Relational Mapper
+# la ORM de ruby on rails es Active record
+
 class Post(models.Model):
   """ Post Model"""
   user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,7 +16,6 @@ class Post(models.Model):
 
   title = models.CharField(max_length = 255)
   photo = models.ImageField(upload_to = 'posts/photos')
-
   created = models.DateTimeField( auto_now_add=True)
   modified = models.DateTimeField(auto_now=True)
 
